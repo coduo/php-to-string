616f6f7dfd47056f39d332d34d9ade395715e6fd
Norbert Orzechowicz <norbert@orzechowicz.pl>
2014-05-07 19:46:57 +0200

    Use PhpSpecDataProviderExtension to improve specs readabilty

--- spec/Coduo/ToString/StringSpec.php
+++ spec/Coduo/ToString/StringSpec.php
@@ -14,46 +14,32 @@ class Foo
 
 class StringSpec extends ObjectBehavior
 {
-    function it_convert_double_to_string()
+    /**
+     * @dataProvider positiveConversionExamples
+     */
+    function it_convert_values_to_string($value, $expectedValue)
     {
-        $this->beConstructedWith(1.1);
-        $this->__toString()->shouldReturn('1.1');
+        $this->beConstructedWith($value);
+        $this->__toString()->shouldReturn($expectedValue);
+    }
+
+    function positiveConversionExamples()
+    {
+        return array(
+            array(1.1, '1.1'),
+            array(20, '20'),
+            array(true, 'true'),
+            array(new \stdClass(), '\stdClass'),
+            array(new Foo(), 'This is Foo'),
+            array(array('foo', 'bar'), 'Array(2)'),
+            array(function() {return 'test';}, '\Closure')
+        );
     }
 
     function it_convert_double_to_string_for_specific_locale()
     {
         $this->beConstructedWith(1.1, 'pl');
         $this->__toString()->shouldReturn('1,1');
-    }
-
-    function it_convert_integer_to_string()
-    {
-        $this->beConstructedWith(20);
-        $this->__toString()->shouldReturn('20');
-    }
-
-    function it_convert_boolean_to_string()
-    {
-        $this->beConstructedWith(true);
-        $this->__toString()->shouldReturn('true');
-    }
-
-    function it_convert_object_to_string()
-    {
-        $this->beConstructedWith(new \stdClass());
-        $this->__toString()->shouldReturn('\stdClass');
-    }
-
-    function it_convert_object_with_toString_method_to_string()
-    {
-        $this->beConstructedWith(new Foo());
-        $this->__toString()->shouldReturn('This is Foo');
-    }
-
-    function it_convert_array_to_string()
-    {
-        $this->beConstructedWith(array('foo', 'bar'));
-        $this->__toString()->shouldReturn('Array(2)');
     }
 
     function it_convert_resource_to_string()
@@ -63,12 +49,5 @@ class StringSpec extends ObjectBehavior
         $this->__toString()->shouldReturn('Resource(stream)');
         fclose($resource);
         unlink(sys_get_temp_dir() . "/foo");
-    }
-
-    function it_convert_callback_string()
-    {
-        $func = function() {return 'test';};
-        $this->beConstructedWith($func);
-        $this->__toString()->shouldReturn('\Closure');
     }
 }
