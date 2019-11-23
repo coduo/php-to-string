46bdbbfa17e07739e7d22f9e86785fda33582da5
norzechowicz <norbert@orzechowicz.pl>
2019-11-23 11:11:40 +0100

    Fixed failing tests

--- spec/Coduo/ToString/StringConverterSpec.php
+++ spec/Coduo/ToString/StringConverterSpec.php
@@ -30,7 +30,7 @@ class StringConverterSpec extends ObjectBehavior
 
     function it_convert_double_to_string_for_specific_locale()
     {
-        $this->beConstructedWith(1.1, 'pl');
+        $this->beConstructedWith(1.1, 'en');
         $this->__toString()->shouldReturn('1,1');
     }
 
