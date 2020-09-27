f28f77be7a65731c2270b3ffa3b1f8a24f717e3f
Norbert Orzechowicz <norbert@orzechowicz.pl>
2020-09-27 12:20:47 +0200

    Added PHP CS Fixer, make test possible to fail at php 8.0

--- spec/Coduo/ToString/StringConverterSpec.php
+++ spec/Coduo/ToString/StringConverterSpec.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace spec\Coduo\ToString;
 
@@ -6,61 +6,63 @@ use PhpSpec\ObjectBehavior;
 
 class StringConverterSpec extends ObjectBehavior
 {
-    function it_convert_float_to_string()
+    public function it_convert_float_to_string() : void
     {
         $this->beConstructedWith(1.1);
         $this->__toString()->shouldReturn('1.1');
     }
 
-    function it_convert_integer_to_string()
+    public function it_convert_integer_to_string() : void
     {
         $this->beConstructedWith(20);
         $this->__toString()->shouldReturn('20');
     }
 
-    function it_convert_boolean_to_string()
+    public function it_convert_boolean_to_string() : void
     {
         $this->beConstructedWith(true);
         $this->__toString()->shouldReturn('true');
     }
 
-    function it_convert_object_std_class_to_string()
+    public function it_convert_object_std_class_to_string() : void
     {
         $this->beConstructedWith(new \stdClass);
         $this->__toString()->shouldReturn('\stdClass');
     }
 
-    function it_convert_object_with_too_string_to_string()
+    public function it_convert_object_with_too_string_to_string() : void
     {
         $this->beConstructedWith(new Foo());
         $this->__toString()->shouldReturn('This is Foo');
     }
 
-    function it_convert_array_to_string()
+    public function it_convert_array_to_string() : void
     {
         $this->beConstructedWith(['foo', 'bar']);
         $this->__toString()->shouldReturn('Array(2)');
     }
 
-    function it_convert_closure_to_string()
+    public function it_convert_closure_to_string() : void
     {
-        $this->beConstructedWith(function() {return 'test';});
+        $this->beConstructedWith(function () {
+            return 'test';
+        });
         $this->__toString()->shouldReturn('\Closure');
     }
 
-    function it_convert_double_to_string_for_specific_locale()
+    public function it_convert_double_to_string_for_specific_locale() : void
     {
         $this->beConstructedWith(1.1, 'en');
         $this->__toString()->shouldReturn('1.1');
     }
 
-    function it_convert_resource_to_string()
+    public function it_convert_resource_to_string() : void
     {
-        $resource = fopen(sys_get_temp_dir() . "/foo", "w");
+        $resource = \fopen(\sys_get_temp_dir() . '/foo', 'w');
         $this->beConstructedWith($resource);
         $this->__toString()->shouldReturn('Resource(stream)');
-        fclose($resource);
-        unlink(sys_get_temp_dir() . "/foo");
+        \fclose($resource);
+        \unlink(\sys_get_temp_dir() . '/foo');
     }
 }
 
